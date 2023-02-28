cb11ed1ca9b3384da985c375226c77f998524ec0
user <user@526-1>
2023-02-28 14:59:00 +0400

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Crypto.AES;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_shifr_Click(object sender, RoutedEventArgs e)
+        {
+            using (AES aes = new AES(tb_key.Text))
+            {
+                string toBeEncrypted = aes.Encrypt(tb_shifr.Text);
+                string encrypted = aes.Encrypt(toBeEncrypted);
+                tb_shifr.Text = encrypted;  
+                
+            }
+
+        }
+
+        private void button_deshifr_Click(object sender, RoutedEventArgs e)
+        {
+            using (AES aes = new AES(tb_key.Text))
+            {
+                string toBeDecrypted = aes.Decrypt(tb_shifr.Text);
+                string decrypted = aes.Decrypt(toBeDecrypted);
+                tb_deshifr.Text = decrypted;
+            }
+        }
+    }
+}
